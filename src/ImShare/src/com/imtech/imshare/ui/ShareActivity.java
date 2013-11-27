6411710c95dce7af90db1ca36a0eeb381f0b8717
suddenlydream <suddenlydream@163.com>
2013-11-28 01:27:32 +0800

    1.完成分享界面的布局；

--- src/ImShare/src/com/imtech/imshare/ui/ShareActivity.java
+++ src/ImShare/src/com/imtech/imshare/ui/ShareActivity.java
@@ -1,7 +1,15 @@
 package com.imtech.imshare.ui;
 
+import com.imtech.imshare.R;
+
 import android.app.Activity;
+import android.os.Bundle;
 
 public class ShareActivity extends Activity{
 	private static final String TAG = "ShareActivity";
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_share);
+	}
 }
